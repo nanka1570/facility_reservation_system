bde3c676204c5339c83b55f88bb677f31a42ae83
nanka1570 <nanka1570@gmail.com>
2026-01-12 18:23:25 +0900

    feat: ユーザー名を表示する機能を追加

--- React/Projects/src/app/dashboard/page.tsx
+++ React/Projects/src/app/dashboard/page.tsx
@@ -2,10 +2,11 @@
 
 import { supabase } from "@/lib/supabase"
 import { useRouter } from "next/navigation" //next.jsの画面遷移
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 export default function Dashboard() {
     const router = useRouter()
+    const [ displayName, setDisplayName ] = useState('')
 
     const handleLogout = async () => {
         const { error } = await supabase.auth.signOut()
@@ -17,8 +18,17 @@ export default function Dashboard() {
     useEffect(() => {
         const checkSession = async () => {
             const { data: { session } } = await supabase.auth.getSession()
+            
             if (!session) {
                 router.push('/')
+            } else {
+                const userId = session.user.id
+                const { data, error } = await supabase
+                    .from('profiles')
+                    .select('*')
+                    .eq('id', userId)
+                    .single()
+                setDisplayName(data.display_name)
             }
             
         }
@@ -29,7 +39,7 @@ export default function Dashboard() {
         <>
             <div>ダッシュボード</div>
             <div>
-                <p>こんにちは、ゲストさん</p>
+                <p>こんにちは、{displayName}さん</p>
                 <button
                 onClick={handleLogout}
                 >ログアウト</button>
